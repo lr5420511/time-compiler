d103f485ca9f8d0c83932d20cc7446337b4772cc
lr5420511 <783621006@qq.com>
2018-09-27 18:32:19 +0800

    1

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { AssertionError } = require('assert');
+const Compiler = require('./lib/compiler');
+
+// 拓展编译器，输出英文版
+
+Compiler.preset = ['', ' and ', ', '];
+
+const formatDuration = seconds => {
+    const compiler = new Compiler({
+        'year': 60 * 60 * 24 * 365,
+        'day': 60 * 60 * 24,
+        'hour': 60 * 60,
+        'minute': 60,
+        'second': 1
+    });
+    try {
+        return compiler.timeify(
+            seconds,
+            (val, name) => {
+                name = `${ val } ${ name }`;
+                if (val > 1) return `${ name }s`;
+                return name;
+            }
+        );
+    } catch (err) {
+        if (err instanceof AssertionError) return 'now';
+    }
+};
+
+module.exports = formatDuration;
